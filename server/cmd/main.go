8a984facedc11ae51d3a0d69cabb7fa694cab09f
Evan Zeimet <podnov@gmail.com>
2021-05-12 19:03:47 -0500

    Calculate earned tokens and determine decimals

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "encoding/json"
 import "fmt"
+import "math"
 import "os"
 
 import "github.com/podnov/bag/server/bscscan"
+import "golang.org/x/text/message"
 
 func main() {
 	accountAddress := "0x5a6d55a598cba3a9fdafd0876c9ca02238c03e38"
@@ -20,8 +21,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Account balance: %d\n", balance)
-
 	transactions, err := client.GetAccountTokenTransactions(accountAddress, tokenAddress)
 
 	if err != nil {
@@ -30,15 +29,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	raw, err := json.MarshalIndent(transactions, "", "	")
+	earned := calculateEarnedTokens(balance, &transactions)
+	decimals := determineTokenDecimal(&transactions)
+	divisor := math.Pow10(decimals)
 
-	if err != nil {
-		fmt.Println(err)
-		fmt.Sprintf("Error: %v", fmt.Sprint(err))
-		os.Exit(1)
-	}
+	printer := message.NewPrinter(message.MatchLanguage("en"))
 
-	fmt.Printf("Account transactions:\n%s\n", raw)
+	formattedBalance := printer.Sprintf("%f", float64(balance) / divisor)
+	formattedEarned := printer.Sprintf("%f", float64(earned) / divisor)
+
+	fmt.Printf("Account balance: %s\n", formattedBalance)
+	fmt.Printf("Earned tokens: %s\n", formattedEarned)
+
 	os.Exit(0)
 }
 
+func calculateEarnedTokens(balance int64, transactions *[]bscscan.TransactionApiResult) (int64) {
+	for _, transaction := range *transactions {
+		balance -= transaction.Value
+	}
+
+	return balance
+}
+
+func determineTokenDecimal(transactions *[]bscscan.TransactionApiResult) (int) {
+	result := -1
+
+	if len(*transactions) > 0 {
+		result = (*transactions)[0].TokenDecimal
+	}
+
+	return result
+}
