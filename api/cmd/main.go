10a49fa56762c6b753ba66f66f6c4701f828cedd
Evan Zeimet <podnov@gmail.com>
2021-05-16 11:02:17 -0500

    Fix CORS

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/podnov/bag/api/controllers"
 )
@@ -21,11 +20,17 @@ func main() {
 }
 
 func createCorsConfig() (gin.HandlerFunc) {
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{
-		"http://*.podnov.com",
-		"httpx://*.podnov.com",
-	}
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://*.podnov.com,https://*podnov.com")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-	return cors.New(config)
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
