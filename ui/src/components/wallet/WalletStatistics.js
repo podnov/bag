99b968037ce656480b28ed964db075a336671be7
Evan Zeimet <podnov@gmail.com>
2021-05-16 17:13:52 -0500

    Add some value formatting

--- ui/src/components/wallet/WalletStatistics.js
+++ ui/src/components/wallet/WalletStatistics.js
@@ -26,6 +26,7 @@ class WalletStatistics extends Component {
 		}
 	}
 
+
 	render() {
 		console.log('render');
 		const isLoading = this.state.isLoading;
@@ -38,16 +39,39 @@ class WalletStatistics extends Component {
 			content = <Loader />;
 		} else if (statistics) {
 			console.log('render statistics');
+
+			let numberFormatter = new Intl.NumberFormat();
+			let currencyFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+
+			let convertPriceSource = function(source) {
+				let result;
+
+				switch (source) {
+					case 'PANCAKE_SWAP_V1':
+						result = 'PancakeSwap v1';
+						break;
+
+					case 'PANCAKE_SWAP_V2':
+						result = 'PancakeSwap v2';
+						break;
+
+					default:
+						result = source;
+				}
+
+				return result;
+			};
+
 			content = <div>
 				<h2>Summary</h2>
 				<ul>
 					<li key="summaryAddress"><label>Address:</label> {statistics.accountAddress}</li>
-					<li key="summaryFirstTransactionAt"><label>First Transaction:</label> {statistics.firstTransactionAt}</li>
-					<li key="summaryTransactionCount"><label>Transaction Count:</label> {statistics.transactionCount}</li>
-					<li key="summaryValue"><label>Value:</label> {statistics.value}</li>
-					<li key="summaryEarnedValue"><label>Earned Value:</label> {statistics.earnedValue}</li>
-					<li key="summaryEarnedValuePerDay"><label>Earned Value Per Day:</label> {statistics.earnedValuePerDay}</li>
-					<li key="summaryEarnedValuePerWeek"><label>Earned Value Per Week:</label> {statistics.earnedValuePerWeek}</li>
+					<li key="summaryFirstTransactionAt"><label>First Transaction:</label> {new Date(statistics.firstTransactionAt).toLocaleString()}</li>
+					<li key="summaryTransactionCount"><label>Transaction Count:</label> {numberFormatter.format(statistics.transactionCount)}</li>
+					<li key="summaryValue"><label>Value:</label> {currencyFormatter.format(statistics.value)}</li>
+					<li key="summaryEarnedValue"><label>Earned Value:</label> {currencyFormatter.format(statistics.earnedValue)}</li>
+					<li key="summaryEarnedValuePerDay"><label>Earned Value Per Day:</label> {currencyFormatter.format(statistics.earnedValuePerDay)}</li>
+					<li key="summaryEarnedValuePerWeek"><label>Earned Value Per Week:</label> {currencyFormatter.format(statistics.earnedValuePerWeek)}</li>
 				</ul>
 				<h2>Tokens</h2>
 				{statistics.tokens.map(function(token, index) {
@@ -55,19 +79,19 @@ class WalletStatistics extends Component {
 						<div>
 							<h3>{token.tokenName} ({token.tokenAddress})</h3>
 							<ul>
-								<li key="{token.tokenAddress}-firstTransactionAt"><label>First Transaction:</label> {token.firstTransactionAt}</li>
-								<li key="{token.tokenAddress}-transationCount"><label>Transaction Count:</label> {token.transactionCount}</li>
+								<li key="{token.tokenAddress}-firstTransactionAt"><label>First Transaction:</label> {new Date(token.firstTransactionAt).toLocaleString()}</li>
+								<li key="{token.tokenAddress}-transationCount"><label>Transaction Count:</label> {numberFormatter.format(token.transactionCount)}</li>
 								<li key="{token.tokenAddress}-tokenPrice"><label>Price:</label> {token.tokenPrice}</li>
-								<li key="{token.tokenAddress}-tokenPriceSource"><label>Price Source:</label> {token.tokenPriceSource}</li>
-								<li key="{token.tokenAddress}-tokenPriceUpdatedAt"><label>Price Updated At:</label> {token.tokenPriceUpdatedAt}</li>
-								<li key="{token.tokenAddress}-tokenCount"><label>Count:</label> {token.tokenCount}</li>
-								<li key="{token.tokenAddress}-value"><label>Value:</label> {token.value}</li>
-								<li key="{token.tokenAddress}-earnedTokenCount"><label>Earned Count:</label> {token.earnedTokenCount}</li>
-								<li key="{token.tokenAddress}-earnedTokenCountPerDay"><label>Earned Count Per Day:</label> {token.earnedTokenCountPerDay}</li>
-								<li key="{token.tokenAddress}-earnedTokenCountPerWeek"><label>Earned Count Per Week:</label> {token.earnedTokenCountPerWeek}</li>
-								<li key="{token.tokenAddress}-earnedValue"><label>Earned Value:</label> {token.earnedValue}</li>
-								<li key="{token.tokenAddress}-earnedValuePerDay"><label>Earned Value Per Day:</label> {token.earnedValuePerDay}</li>
-								<li key="{token.tokenAddress}-earnedValuePerWeek"><label>Earned Value Per Week:</label> {token.earnedValuePerWeek}</li>
+								<li key="{token.tokenAddress}-tokenPriceSource"><label>Price Source:</label> {convertPriceSource(token.tokenPriceSource)}</li>
+								<li key="{token.tokenAddress}-tokenPriceUpdatedAt"><label>Price Updated At:</label> {new Date(token.tokenPriceUpdatedAt).toLocaleString()}</li>
+								<li key="{token.tokenAddress}-tokenCount"><label>Count:</label> {numberFormatter.format(token.tokenCount)}</li>
+								<li key="{token.tokenAddress}-value"><label>Value:</label> {currencyFormatter.format(token.value)}</li>
+								<li key="{token.tokenAddress}-earnedTokenCount"><label>Earned Count:</label> {numberFormatter.format(token.earnedTokenCount)}</li>
+								<li key="{token.tokenAddress}-earnedTokenCountPerDay"><label>Earned Count Per Day:</label> {numberFormatter.format(token.earnedTokenCountPerDay)}</li>
+								<li key="{token.tokenAddress}-earnedTokenCountPerWeek"><label>Earned Count Per Week:</label> {numberFormatter.format(token.earnedTokenCountPerWeek)}</li>
+								<li key="{token.tokenAddress}-earnedValue"><label>Earned Value:</label> {currencyFormatter.format(token.earnedValue)}</li>
+								<li key="{token.tokenAddress}-earnedValuePerDay"><label>Earned Value Per Day:</label> {currencyFormatter.format(token.earnedValuePerDay)}</li>
+								<li key="{token.tokenAddress}-earnedValuePerWeek"><label>Earned Value Per Week:</label> {currencyFormatter.format(token.earnedValuePerWeek)}</li>
 							</ul>
 						</div>
 					)
