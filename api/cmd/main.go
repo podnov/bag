bea97c9ed6ce4ded3a52a2b205bfb70d657f7633
Evan Zeimet <podnov@gmail.com>
2021-05-16 09:23:29 -0500

    Add CORS allow config

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/podnov/bag/api/controllers"
 )
 
 func main() {
 	r := gin.Default()
+
+	corsConfig := createCorsConfig()
+	r.Use(corsConfig)
 
 	r.GET("/", controllers.CheckRoot)
 	r.GET("/bag/api/v1/accounts/:accountId", controllers.GetAccount) 
@@ -16,3 +20,12 @@ func main() {
 	r.Run()
 }
 
+func createCorsConfig() (gin.HandlerFunc) {
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{
+		"http://*.podnov.com",
+		"httpx://*.podnov.com",
+	}
+
+	return cors.New(config)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/podnov/bag
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.1 // indirect
 	github.com/go-resty/resty/v2 v2.6.0 // indirect
 	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
