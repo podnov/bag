9157a0184a4b191e378a8346287199218360c4a8
Evan Zeimet <podnov@gmail.com>
2021-05-16 19:39:23 -0500

    Add popup disclaimer

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Modal from 'react-modal';
 import './App.css';
 import WalletInput from './components/wallet/WalletInput';
 import WalletStatistics from './components/wallet/WalletStatistics';
@@ -7,10 +8,33 @@ import AdSenseWidget from './components/adsense/AdSenseWidget';
 
 function App() {
 
+	Modal.setAppElement('*');
+	const [ modalIsOpen, setModalIsOpen ] = React.useState(true);
+
+	const customModalStyles = {
+		content : {
+			top : '50%',
+			left : '50%',
+			right : 'auto',
+			bottom : 'auto',
+			marginRight : '-50%',
+			transform : 'translate(-50%, -50%)'
+		}
+	};
+
+	function closeModal(){
+		setModalIsOpen(false);
+	}
+
+	function openModal(){
+		setModalIsOpen(true);
+	}
+
 	return (
 		<div className="App">
 			<header>
 				<h1>CryptoBag</h1>
+				<div><button onClick={openModal}>Show Disclaimers</button></div>
 				<WalletInput />
 			</header>
 			<div className="content">
@@ -25,6 +49,30 @@ function App() {
 				</div>
 				<AdSenseWidget />
 			</footer>
+
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				style={customModalStyles}
+				contentLabel="Disclaimers"
+				shouldCloseOnOverlayClick={false}
+				>
+				<h2>Disclaimers</h2>
+				<ul>
+					<li>The creator(s) of this website are not financial advisors</li>
+					<li>Token past performance is not an indicator of future performance</li>
+					<li>The materials and information on this website are intended for informational purposes only. Any information on this site may be out of date.</li>
+					<li>Information provided on this website is provided on an "as is" basis without warranty of any kind, either express or implied</li>
+					<li>All monetary figures are shown in USD and are not intended to be used for any official purposes (i.e. taxes).</li>
+					<li>All prices are provided by an external provider (PancakeSwap v1 or v2 at this time)</li>
+					<li>Earned tokens are simplified as a calculation similar to (earned = current - bought + sold) and may not be entirely accurate</li>
+					<li>Earned tokens and values over time (per day or per week) are simplified calculations averaging earned amount over the time since your first transaction for that token</li> 
+					<li>This site is intended to be used with Binance Smart Chain wallets only</li>
+					<li>By using this website you agree that the creators cannot be held liable in respect to actions taken or not taken based on information contained on or missing from this website.</li>
+				</ul>
+				<div className="modalButtons"><button onClick={closeModal}>Agreed</button></div>
+			</Modal>
+
 		</div>
 	);
 }
