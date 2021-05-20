154b4433678e4431c91acf06ad4f0072c0155147
Evan Zeimet <podnov@gmail.com>
2021-05-19 21:18:46 -0500

    update disclaimers

--- ui/src/App.js
+++ ui/src/App.js
@@ -66,18 +66,13 @@ function App() {
 				>
 				<h2>Disclaimers</h2>
 				<ul>
-					<li>The creator(s) of this website are not financial advisors</li>
-					<li>Token past performance is not an indicator of future performance</li>
-					<li>This site is intended to be used with Binance Smart Chain wallets only</li>
-					<li>The materials and information on this website are intended for informational purposes only. Any information on this site may be out of date.</li>
-					<li>Information provided on this website is provided on an "as is" basis without warranty of any kind, either express or implied</li>
-					<li>All monetary figures are shown in USD and are not intended to be used for any official purposes (i.e. taxes).</li>
-					<li>All prices are provided by an external provider (PancakeSwap v1 or v2 at this time), and these are often delayed</li>
+					<li>The creator(s) of this website are not financial advisors and nothing on this site should be viewed as financial advice.</li>
+					<li>Information provided on this website is provided on an "as is" basis without warranty of any kind, either express or implied. The materials and information on this website are intended for informational purposes only. Any information on this site may be out of date.</li>
+					<li>Token past performance is not an indicator of future performance.</li>
+					<li>This site is intended to be used with Binance Smart Chain wallets only.</li>
+					<li>All monetary figures are shown in USD and are not intended to be used for any official purposes (i.e. taxes). All prices are provided by an external provider (PancakeSwap v1 or v2 at this time), and these are often delayed</li>
 					<li>If you have over 10,000 transactions on your account, this site is NOT for you at this time due to API response size limits</li>
-					<li>Accrued tokens and values refer to those tokens accrued through something like transaction tax-based reflection to holders</li> 
-					<li>Accrued tokens and values calculations are likely to be skewed by recent transaction (&lt; 1hr old) due to transaction API lag</li>
-					<li>Accrued tokens are simplified as a calculation similar to (accrued = current - bought + sold) and may not be entirely accurate in all scenarios</li>
-					<li>Accrued tokens and values over time (per day or per week) are simplified calculations averaging accrued amount over the time since your first transaction for that token</li> 
+					<li>Accrued tokens and values refer to those tokens accrued through something like transaction tax-based reflection to holders. Accrued tokens and values calculations are likely to be skewed by recent transaction (&lt; 1hr old) due to transaction API lag. Accrued tokens are simplified as a calculation similar to (accrued = current - bought + sold) and may not be entirely accurate in all scenarios. Accrued tokens and values over time (per day or per week) are simplified calculations averaging accrued amount over the time since your first transaction for that token</li> 
 					<li>By using this website you agree that the creator(s) cannot be held liable in respect to actions taken or not taken based on information contained on or missing from this website.</li>
 				</ul>
 				<div className="modalButtons"><button onClick={closeModal}>Agreed</button></div>
